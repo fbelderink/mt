8da00f1adc7f168f9a494699cf0101cffac60ea3
fbelderink <fynnmaarten.business@gmail.com>
2024-05-21 21:08:47 +0200

    reworked batching/fragmenting to support pytorch datasets

--- assignments/assignment2.py
+++ assignments/assignment2.py
@@ -11,16 +11,13 @@ def task_evaluate(multi30k_de: List[List[str]], multi30k_en: List[List[str]]):
     joint_set.extend(multi30k_de)
 
     for num_ops in [1000, 5000, 15000]:
-        dic1 = Dictionary()
-        dic1.generate_vocabulary(multi30k_de, generate_bpe(multi30k_de, num_ops))
+        dic1 = Dictionary(multi30k_de, generate_bpe(multi30k_de, num_ops))
         print(f"Number of unique words with {num_ops} operations on multi30k.de: {len(dic1)}")
 
-        dic2 = Dictionary()
-        dic2.generate_vocabulary(multi30k_en, generate_bpe(multi30k_en, num_ops))
+        dic2 = Dictionary(multi30k_en, generate_bpe(multi30k_en, num_ops))
         print(f"Number of unique words with {num_ops} operations on multi30k.en: {len(dic2)}")
 
-        dic3 = Dictionary()
-        dic3.generate_vocabulary(joint_set, generate_bpe(joint_set, num_ops))
+        dic3 = Dictionary(joint_set, generate_bpe(joint_set, num_ops))
         print(f"Number of unique words with {num_ops} operations on multi30k.de joint with multi30k.en: {len(dic3)}")
 
         print()
@@ -42,11 +39,9 @@ def task_batches(window_size, batch_size, multi30k_de, multi30k_en, lines=(1100,
 
     save_batches('eval/string_batches_old', batches)
 
-    dic_de = Dictionary()
-    dic_de.generate_vocabulary([], german_ops, transformed_words=transformed_words_de)
+    dic_de = Dictionary([], german_ops, transformed_words=transformed_words_de)  # generate from transformed words
 
-    dic_en = Dictionary()
-    dic_en.generate_vocabulary([], english_ops, transformed_words=transformed_words_en)
+    dic_en = Dictionary([], english_ops, transformed_words=transformed_words_en)
 
     index_batches = get_index_batches(batches, dic_de, dic_en)
     save_batches('eval/index_batches_old', index_batches)
---
+++ preprocessing/fragment.py
@@ -0,0 +1,165 @@
+from typing import List
+from preprocessing.dictionary import Dictionary
+import numpy as np
+
+
+def _get_alignment(index: int, target_len: int, source_len: int) -> int:
+    """
+    Maps every target position to exactly one source position, considering following constraints:
+    1. monotony: b_i \leq b_{i + 1}
+    2. EOS to EOS: b_{I + 1} = J + 1, where I + 1 = target_len and J + 1 = source_len
+
+    Args:
+        index: index in the
+        target_len:
+        source_len:
+
+    Returns: aligned (source) index for a given target index
+
+    """
+    #  align start of sentence
+    if index == 0:
+        return 0
+
+    #  align end of sentence (second constraint)
+    if index == target_len:
+        return source_len
+
+    #  equally distribute indices in remaining space
+    dist_factor = (source_len - 1) / (target_len - 1)
+
+    return int(round(index * dist_factor))
+
+
+def _append_eos_to_sentences(data: List[List[str]]):
+    """
+    Appends end of sentence (EOS) symbol to every sentence.
+    Args:
+        data: List of sentences to append EOS to
+
+    Returns: List of sentences with EOS symbol appended.
+
+    """
+    return [sentence + ['</s>'] for sentence in data]
+
+
+def _get_target_labels(target_data: List[List[str]]):
+    """
+    Flattens input list and reshapes it to vector form
+    Args:
+        target_data: List of sentences to get label vector for
+
+    Returns: numpy array of shape (N, 1) with target labels
+
+    """
+    #  flatten data and reshape to shape (N, 1)
+    flattened_data = np.hstack(target_data).reshape(-1, 1)
+
+    return flattened_data
+
+
+def _get_padded_sentence(sentence: List[str], indices: tuple):
+    """
+    Pads sentence if needed with start of sentence (SOS) and end of sentence (EOS) according to indices.
+    Args:
+        sentence:
+        indices: start and end indices of subsentence
+
+    Returns: subsentence specified by indices, padded if needed
+
+    """
+
+    padded_sentence = []
+
+    # pad with SOS if first index is below 0
+    if indices[0] <= 0:
+        padded_sentence = ['<s>'] * (abs(indices[0]) + 1)
+
+    if indices[1] <= len(sentence):
+        # append requested subsentence
+        padded_sentence += sentence[max(0, indices[0] - 1): indices[1]]
+    else:
+        # append requested subsentence and pad with EOS if last index is above sentence length
+        padded_sentence += sentence[max(0, indices[0] - 1):] + (['</s>'] * (indices[1] - len(sentence)))
+
+    return padded_sentence
+
+
+def _get_target_window_matrix(target_data: List[List[str]], window_size: int):
+    """
+    Calculates target window matrix for given target data and window size
+    Args:
+        target_data: List of target sentences
+        window_size: context restriction window
+
+    Returns: target window matrix with shape (N, window_size), N is determined by the len of target data
+
+    """
+    word_matrices = []
+    for sentence in target_data:
+        # create empty matrix to be filled with words
+        word_matrix = np.empty((len(sentence), window_size), dtype=object)
+
+        for idx, word in enumerate(sentence):
+            # place subsentence into idx-th row of matrix, according to definition
+            word_matrix[idx, :] = _get_padded_sentence(sentence, (idx + 1 - window_size, idx))
+
+        word_matrices.append(word_matrix)
+
+    # return vertical stacked matrix of word matrices list
+    return np.vstack(word_matrices)
+
+
+def _get_source_window_matrix(source_data: List[List[str]], target_data: List[List[str]], window_size: int):
+    """
+    Calculates source window matrix for given source and target data and window size
+
+    Args:
+        source_data: List of source sentences, where each sentence is a list of words
+        target_data: List of target sentences, where each sentence is a list of words
+        window_size: context restriction window
+
+    Returns: source window matrix with shape (N, window_size), N is determined by the len of target data
+
+    """
+    word_matrices = []
+
+    for source_sentence, target_sentence in zip(source_data, target_data):
+        # create empty matrix to be filled with words
+        word_matrix = np.empty((len(target_sentence), 2 * window_size + 1), dtype=object)
+        for i in range(len(target_sentence)):
+            # calculate alignment of index with given target and source sentence length
+            b_i = _get_alignment(i, len(target_sentence), len(source_sentence))
+            # get (padded) subsentence considering alignment and fill content into matrix row
+            word_matrix[i, :] = _get_padded_sentence(source_sentence, (b_i - window_size, b_i + window_size))
+
+        word_matrices.append(word_matrix)
+
+    # return vertical stacked matrix of word matrices list
+    return np.vstack(word_matrices)
+
+
+def fragment_data(source_data: List[List[str]], target_data: List[List[str]], window_size: int) -> tuple:
+    # add end of sentence symbols to sentences
+    target_data_with_eos = _append_eos_to_sentences(target_data)
+
+    # calculate matrices for complete data
+    source_window_mat = _get_source_window_matrix(source_data, target_data_with_eos, window_size)
+    target_labels = _get_target_labels(target_data_with_eos)
+    target_window_mat = _get_target_window_matrix(target_data_with_eos, window_size)
+
+    return source_window_mat, target_window_mat, target_labels
+
+
+def fragment_data_to_indices(source_data: List[List[str]],
+                             target_data: List[List[str]],
+                             window_size: int,
+                             source_dict: Dictionary,
+                             target_dict: Dictionary):
+    S, T, L = fragment_data(source_data, target_data, window_size)
+
+    get_source_idx = np.vectorize(source_dict.get_index_of_string)
+    get_target_idx = np.vectorize(target_dict.get_index_of_string)
+
+    return get_source_idx(S), get_target_idx(T), get_target_idx(L)
+
