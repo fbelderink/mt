59c6e2cb16eb62e53ba57cae1355ef4a4d89cce1
fbelderink <fynnmaarten.business@gmail.com>
2024-06-10 15:15:14 +0200

    added fabians changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 from utils.file_manipulation import load_data
 from assignments.assignment3 import *
+from assignments.assignment2 import *
+from assignments.assignment4 import *
 import training.train as train
 from utils.ConfigLoader import ConfigLoader
 from utils.hyperparameters import Hyperparameters
@@ -21,15 +23,21 @@ def _parse_arguments() -> argparse.Namespace:
 if __name__ == "__main__":
     args = _parse_arguments()
 
-    #multi30k_de = load_data(args.hyps)
-    #multi30k_en = load_data(args.refs)
+    multi30k_de = load_data(args.hyps)
+    multi30k_en = load_data(args.refs)
 
-    #dict_de = Dictionary.load("data/train_dict_de.pkl")
-    #dict_en = Dictionary.load("data/train_dict_en.pkl")
+    #task_batches(args.window_size, args.batch_size, multi30k_de, multi30k_en)
+
+    dict_de = Dictionary.load("data/dicts/train_dict_de.pkl")
+    dict_en = Dictionary.load("data/dicts/train_dict_en.pkl")
 
     #generate dataset
     #generate_dataset(multi30k_de, multi30k_en, args.window_size, 7000,
-    #                 save_path='data/val7k.pt', dict_de=dict_de, dict_en=dict_en)
+    #                 dict_de=dict_de, dict_en=dict_en, save_path='data/val7k.pt')
     #test_dataset_load('data/train7k.pt')
+
     train.train("data/train7k.pt", "data/val7k.pt",
                 Hyperparameters(ConfigLoader("configs/config.yaml").get_config()))
+
+
+    #test_beam_search()
--- search/greedy_search.py
+++ search/greedy_search.py
@@ -1,20 +1,19 @@
 from utils.file_manipulation import load_data
 from utils.file_manipulation import save_data
-from preprocessing.dictionary import Dictionary 
+from preprocessing.dictionary import Dictionary
 import torch
 from model.basic_net import BasicNet
 from utils.hyperparameters import Hyperparameters
 from utils.ConfigLoader import ConfigLoader
-from preprocessing import fragment 
+from preprocessing import fragment
 import numpy as np
 from preprocessing.postprocessing import undo_prepocessing
 
 
-
 def translate(model_path: str, source_data_path: str, target_data_path: str,
-               source_dict_path: Dictionary, target_dict_path: Dictionary, config: Hyperparameters):
-
+              source_dict_path: Dictionary, target_dict_path: Dictionary, config: Hyperparameters):
     # TODO change checkpointing so no hyperparameters need to be hardcoded
+    # TODO experiment with bigger window size
     window_size = 2
 
     # TODO
@@ -30,24 +29,29 @@ def translate(model_path: str, source_data_path: str, target_data_path: str,
     model.eval()
 
     # preprocessed source sentences   
-    source_data = source_dict.apply_vocabulary_to_text(load_data(source_data_path),bpe_performed=False)
+    source_data = source_dict.apply_vocabulary_to_text(load_data(source_data_path), bpe_performed=False)
 
     target_sentences = []
 
     for sentence in source_data:
+        # gives the model more flexibility on when to end the sentence
+        number_eos = 5
+        for i in range(number_eos):
+            sentence.append('</s>')
 
         # contains all entries needed to translate current sentence
-        S = fragment._get_source_window_matrix([sentence], [[0]*(len(sentence)*alignment_factor+1)], window_size)
+        S = fragment._get_source_window_matrix([sentence], [[0] * (len(sentence) * alignment_factor + 1)], window_size)
+
         print(S)
         get_source_idx = np.vectorize(source_dict.get_index_of_string)
-        S = torch.from_numpy(get_source_idx(S)) # replace tokens with corresponding indices
+        S = torch.from_numpy(get_source_idx(S))  # replace tokens with corresponding indices
 
         # contains the entry needed to translate the next sentence 
         t_list = ['<s>'] * window_size
 
         get_target_idx = np.vectorize(target_dict.get_index_of_string)
-        t_tensor = torch.from_numpy(get_target_idx(t_list)) # replace tokens with corresponding indices
-        
+        t_tensor = torch.from_numpy(get_target_idx(t_list))  # replace tokens with corresponding indices
+
         translated_sentence = []
 
         for s in S:
@@ -62,29 +66,22 @@ def translate(model_path: str, source_data_path: str, target_data_path: str,
             s = s.to(device)
             t_tensor = t_tensor.to(device)
 
-
             pred = model(s, t_tensor)
             res = target_dict.get_string_at_index(int(torch.argmax(pred)))
             translated_sentence.append(res)
-            
+
             # update t
             t_list.append(res)
             t_list.pop(0)
             t_tensor = torch.from_numpy(get_target_idx(t_list))
-        
+
         target_sentences.append(translated_sentence)
-        print(translated_sentence) 
-    
+        print(translated_sentence)
 
     target_sentences = undo_prepocessing(target_sentences)
-    save_data(target_data_path,target_sentences)
+    save_data(target_data_path, target_sentences)
 
 
-
-
-
-
-
-
-
-translate("eval/checkpoints/09-06-2024/18_01_21.pth","data_translation/source.txt","data_translation/target.txt","data/train_dict_de.pkl","data/train_dict_en.pkl",Hyperparameters(ConfigLoader("configs/config.yaml").get_config()))
+translate("eval/checkpoints/09-06-2024/18_01_21.pth", "data_translation/source.txt", "data_translation/target.txt",
+          "data/train_dict_de.pkl", "data/train_dict_en.pkl",
+          Hyperparameters(ConfigLoader("configs/config.yaml").get_config()))
