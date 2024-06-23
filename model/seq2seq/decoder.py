fixed accuracy calculation ig

model/seq2seq/decoder.py
@@ -74,18 +74,12 @@ class AttentionDecoder(nn.Module):
                 teacher_forcing=False, apply_log_softmax=True):
 
         prob_dists = []
-        # expected shape (B x 1)
+        # expected target_tensor shape: (B x seq_len)
+        target_word = target_tensor[:, 1].unsqueeze(1)
 
         prev_decoder_state = encoder_state
 
-        decoder_predictions = torch.ones(target_tensor.size(0), 1).to(target_tensor.device) * START
-
-        for k in range(target_tensor.size(1) - 1):
-
-            if teacher_forcing:
-                target_word = target_tensor[:, k].unsqueeze(1)
-            else:
-                target_word = decoder_predictions
+        for k in range(1, target_tensor.size(1)):
 
             fc_out, prev_decoder_state = self.forward_step(encoder_outputs,
                                                            prev_decoder_state,
@@ -93,7 +87,10 @@ class AttentionDecoder(nn.Module):
                                                            apply_log_softmax)
             # fc_out shape (B x 1 x target_dict_size)
 
-            decoder_predictions = torch.argmax(fc_out, dim=-1)
+            if teacher_forcing:
+                target_word = target_tensor[:, k].unsqueeze(1)
+            else:
+                target_word = torch.argmax(fc_out, dim=-1)
 
             prob_dists.append(fc_out)
         return torch.cat(prob_dists, dim=1)

training/train.py
@@ -121,6 +121,8 @@ def train_epoch(model, train_dataloader, validation_dataloader,
                                                         label)
             print(f"steps: {steps}, epoch: {epoch_num}")
             print(f"batch metrics: accuracy: {accuracy}, perplexity: {perplexity}, loss: {loss.item()}\n")
+            print(torch.argmax(predictions, dim=1)[1])
+            print(label[1])
         if train_params.test_model_every != 0 and steps % train_params.test_model_every == 0:
             val_ppl, val_acc = test_on_validation_data(model, validation_dataloader, train_params)
 
@@ -175,7 +177,7 @@ def test_on_validation_data(model, validation_dataloader, train_params):
 def forward_pass(model, source, target, label, train_params):
     if isinstance(train_params, RNNTrainHyperparameters):
         predictions = model(source, target,
-                            teacher_forcing=train_params.teacher_forcing,)
+                            teacher_forcing=train_params.teacher_forcing)
     elif isinstance(train_params, FFTrainHyperparameters):
         predictions = model(source, target)
         predictions = predictions.unsqueeze(-1)
@@ -187,12 +189,12 @@ def forward_pass(model, source, target, label, train_params):
     return predictions, loss
 
 
-def evaluate_performance(predictions, loss, L):
+def evaluate_performance(predictions, loss, labels):
     # predictions shape (B x dict_size x seq_len)
     # L shape (B x seq_len)
     correct = 0
     samples = 0
-    for ps, ls in zip(torch.argmax(predictions, dim=1), L):
+    for ps, ls in zip(torch.argmax(predictions, dim=1), labels):
         for p, l in zip(ps, ls):
             if l != PADDING:
                 # only count matching non eos symbols