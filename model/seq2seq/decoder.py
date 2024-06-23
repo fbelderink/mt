52b4620e5990d064a51e66e0a81d35dc3886c60b
fbelderink <fynnmaarten.business@gmail.com>
2024-06-23 19:03:01 +0200

    introduce gru option and refactored decoder forward

--- model/seq2seq/decoder.py
+++ model/seq2seq/decoder.py
@@ -2,96 +2,130 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from model.layers.attention import Attention
+from utils.types import RNNType
+from preprocessing.dictionary import START, PADDING
 
-
+"""
 class Decoder(nn.Module):
+    def __init__(self, target_dict_size, embed_dim):
+        self.embedding = nn.Embedding(target_dict_size,
+                                      embed_dim,
+                                      padding_idx=PADDING)
 
-    def __init__(self, target_dict_size,
-                 embed_dim,
-                 lstm_hidden,
-                 lstm_layers,
-                 lstm_dropout,
-                 lstm_bidirectional):
-        super(Decoder, self).__init__()
+        self.gru = nn.GRU(embed_dim, embed_dim, batch_first=True)
 
-        self.embedding = nn.Embedding(target_dict_size, embed_dim)
+    def forward(self, encoder_output, hidden, target):
 
-        self.lstm = nn.LSTM(
-            embed_dim,
-            lstm_hidden,
-            lstm_layers,
-            dropout=lstm_dropout,
-            bidirectional=lstm_bidirectional,
-            batch_first=True
-        )
 
-    def forward(self, target_word, state):
-        embedded = self.embedding(target_word)
 
-        decoder_output, state = self.lstm(embedded, state)
+        for i in range(target.size(1) - 1):
+            pass
 
-        return decoder_output, state
+    def step_forward(self, encoder_output, prev_state, target_tensor):
+        pass
+
+"""
 
 
 class AttentionDecoder(nn.Module):
     def __init__(self,
+                 rnn_type: RNNType,
                  target_dict_size,
                  embed_dim,
-                 lstm_hidden,
-                 lstm_layers,
-                 lstm_dropout,
-                 lstm_bidirectional):
+                 hidden,
+                 layers,
+                 dropout,
+                 bidirectional,
+                 use_attention=True):
 
         super(AttentionDecoder, self).__init__()
 
-        self.decoder = Decoder(target_dict_size,
-                               embed_dim,
-                               lstm_hidden,
-                               lstm_layers,
-                               lstm_dropout,
-                               lstm_bidirectional)
+        self.embedding = nn.Embedding(target_dict_size,
+                                      embed_dim,
+                                      padding_idx=PADDING)
 
-        self.attention = Attention(lstm_hidden, lstm_bidirectional)
+        num_directions = 2 if bidirectional else 1
 
-        if lstm_bidirectional:
-            fc_input_size = 2 * 2 * lstm_hidden
+        self.rnn_type = rnn_type
+
+        if self.rnn_type == RNNType.GRU:
+            self.rnn = nn.GRU(embed_dim + num_directions * hidden,
+                              hidden,
+                              layers,
+                              dropout=dropout,
+                              bidirectional=bidirectional,
+                              batch_first=True)
         else:
-            fc_input_size = 2 * lstm_hidden
+            self.rnn = nn.LSTM(embed_dim + num_directions * hidden,
+                               hidden,
+                               layers,
+                               dropout=dropout,
+                               bidirectional=bidirectional,
+                               batch_first=True)
 
-        self.fc = nn.Linear(fc_input_size, target_dict_size)
+        self.use_attention = use_attention
 
-    def forward(self, encoder_outputs, encoder_state, target_tensor, T_max=-1,
+        if self.use_attention:
+            self.attention = Attention(num_directions * hidden, hidden)
+
+        self.fc = nn.Linear(num_directions * hidden, target_dict_size)
+
+    def forward(self, encoder_outputs, encoder_state, target_tensor,
                 teacher_forcing=False, apply_log_softmax=True):
 
         prob_dists = []
-        target_word = target_tensor[:, 0].unsqueeze(1)
+        # expected shape (B x 1)
 
-        state = encoder_state
+        prev_decoder_state = encoder_state
 
-        T_max = max(T_max, target_tensor.shape[1])
-        for k in range(1, T_max):
-            fc_out, state = self.forward_step(encoder_outputs, state, target_word, apply_log_softmax)
+        decoder_predictions = torch.ones(target_tensor.size(0), 1).to(target_tensor.device) * START
+
+        for k in range(target_tensor.size(1) - 1):
 
             if teacher_forcing:
                 target_word = target_tensor[:, k].unsqueeze(1)
             else:
-                max_idx = torch.argmax(fc_out, dim=-1)
-                target_word = max_idx.unsqueeze(1).detach()
+                target_word = decoder_predictions
+
+            fc_out, prev_decoder_state = self.forward_step(encoder_outputs,
+                                                           prev_decoder_state,
+                                                           target_word,
+                                                           apply_log_softmax)
+            # fc_out shape (B x 1 x target_dict_size)
+
+            decoder_predictions = torch.argmax(fc_out, dim=-1)
 
             prob_dists.append(fc_out)
+        return torch.cat(prob_dists, dim=1)
 
-        return torch.stack(prob_dists).permute(1, 0, 2)
+    def forward_step(self, encoder_outputs, prev_state, target_word, apply_log_softmax=True):
+        # encoder_outputs shape: (B x seq_len x hidden * directions)
+        # hidden_state shape: (B x 1 x hidden * directions)
+        embedded = self.embedding(target_word)
+        embedded = F.relu(embedded)
+        # embedded shape (B x 1 x embed_dim)
 
-    def forward_step(self, encoder_outputs, state, target_word, apply_log_softmax=True):
-        decoder_outputs, state = self.decoder(target_word, state)
+        if self.use_attention:
+            if self.rnn_type == RNNType.GRU:
+                prev_hidden_state = prev_state
+            else:
+                (prev_hidden_state, _) = prev_state
 
-        context_vector = self.attention(encoder_outputs, decoder_outputs)
+            last_hidden_state = prev_hidden_state.permute(1, 0, 2)[:, -1, :].unsqueeze(1)
+            # last_hidden_state shape (B x 1 x lstm_hidden)
+            context_vector = self.attention(encoder_outputs, last_hidden_state)
+            # context_vector shape (B x 1 x encoder_hidden)
+        else:
+            context_vector = encoder_outputs[:, -1, :].unsqueeze(1)
 
-        decoder_outputs = decoder_outputs.squeeze(1)
+        concat = torch.cat((embedded, context_vector), dim=-1)
+        # concat shape (B x 1 x (embed_dim + encoder_hidden))
+        decoder_outputs, prev_state = self.rnn(concat, prev_state)
+        # decoder_outputs shape (B x 1 x lstm_hidden * num_directions)
 
-        fc_out = self.fc(torch.cat((context_vector, decoder_outputs), dim=-1))
+        fc_out = self.fc(decoder_outputs)
 
         if not self.training and apply_log_softmax:
             fc_out = F.log_softmax(fc_out, dim=-1)
 
-        return fc_out, state
+        return fc_out, prev_state
--- model/seq2seq/recurrent_net.py
+++ model/seq2seq/recurrent_net.py
@@ -3,43 +3,59 @@ from model.basic_net import BasicNet
 from model.seq2seq.encoder import Encoder
 from model.seq2seq.decoder import AttentionDecoder
 from utils.hyperparameters.model_hyperparameters import RNNModelHyperparameters
+from preprocessing.dictionary import PADDING
+from utils.types import RNNType
 
 
 class RecurrentNet(BasicNet):
-    def __init__(self, source_dict_size, target_dict_size, config: RNNModelHyperparameters, eos_idx, model_name="rnn"):
+    def __init__(self,
+                 source_dict_size,
+                 target_dict_size,
+                 config: RNNModelHyperparameters,
+                 model_name="rnn"):
         super(RecurrentNet, self).__init__(source_dict_size, target_dict_size, config, model_name)
 
-        self.encoder = Encoder(source_dict_size,
+        if config.rnn_type == 'lstm':
+            rnn_type = RNNType.LSTM
+        elif config.rnn_type == 'gru':
+            rnn_type = RNNType.GRU
+        else:
+            raise ValueError("Unsupported rnn type")
+
+        self.encoder = Encoder(rnn_type,
+                               source_dict_size,
                                config.encoder_parameters[0],
-                               config.lstm_hidden_dim,
-                               config.lstm_layers,
+                               config.rnn_hidden_dim,
+                               config.rnn_layers,
                                config.encoder_parameters[1],
-                               config.lstm_bidirectional)
+                               config.rnn_bidirectional)
 
-        self.decoder = AttentionDecoder(target_dict_size,
+        self.decoder = AttentionDecoder(rnn_type,
+                                        target_dict_size,
                                         config.decoder_parameters[0],
-                                        config.lstm_hidden_dim,
-                                        config.lstm_layers,
+                                        config.rnn_hidden_dim,
+                                        config.rnn_layers,
                                         config.decoder_parameters[1],
-                                        config.lstm_bidirectional)
+                                        config.rnn_bidirectional,
+                                        config.use_attention)
 
-        self.criterion = nn.CrossEntropyLoss(ignore_index=eos_idx)
+        self.criterion = nn.CrossEntropyLoss(ignore_index=PADDING)
 
-    def forward(self, S, T, T_max=-1,
+    def forward(self, source, target,
                 teacher_forcing=False, apply_log_softmax=True):
+        encoder_outputs, encoder_state = self.encoder(source)
 
-        encoder_outputs, encoder_state = self.encoder(S)
-        decoder_outputs = self.decoder(encoder_outputs, encoder_state, T,
+        decoder_outputs = self.decoder(encoder_outputs, encoder_state, target,
                                        teacher_forcing=teacher_forcing,
-                                       T_max=T_max,
                                        apply_log_softmax=apply_log_softmax)
 
         return decoder_outputs
 
-    def compute_loss(self, pred, L):
+    def compute_loss(self, pred, label):
         pred = pred.permute(0, 2, 1)
+        # pred shape (B x target_dict_size x seq_len)
 
-        return self.criterion(pred, L)
+        return self.criterion(pred, label)
 
     def get_encoder(self):
         return self.encoder
