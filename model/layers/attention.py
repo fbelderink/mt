7dc981abc2588f7938f769464fc71b58eb257667
fbelderink <fynnmaarten.business@gmail.com>
2024-06-23 19:02:30 +0200

    refactored attention to take prev decoder hidden state instead of decoder output

--- model/layers/attention.py
+++ model/layers/attention.py
@@ -8,35 +8,30 @@ class Attention(nn.Module):
     Attention layer, implemented as proposed in Bahdanau & Cho
     """
 
-    def __init__(self, hidden, bidirectional_lstms):
+    def __init__(self, encoder_hidden, decoder_hidden):
         super(Attention, self).__init__()
 
-        if bidirectional_lstms:
-            input_size = 2 * 2 * hidden
-        else:
-            input_size = 2 * hidden  # encoder_hidden + decoder_hidden
-
-        self.fc_1 = nn.Linear(input_size, hidden)
+        self.Wa = nn.Linear(decoder_hidden, encoder_hidden)
+        self.Ua = nn.Linear(encoder_hidden, encoder_hidden)
         self.activation = nn.Tanh()
 
-        self.v_a = nn.Linear(hidden, 1)
+        self.Va = nn.Linear(encoder_hidden, 1)
 
-    def forward(self, encoder_outputs, decoder_outputs):
-        # encoder_outputs shape (B x L x hidden)
-        # decoder_outputs shape (B x 1 x hidden)
-        decoder_outputs = decoder_outputs.repeat(1, encoder_outputs.size(1), 1)
+    def forward(self, encoder_outputs, decoder_hidden):
+        # encoder_outputs shape (B x L x encoder_hidden)
+        # decoder_hidden shape (B x 1 x decoder_hidden)
+        Wa = self.Wa(decoder_hidden)
+        # expected shape (B x 1 x encoder_hidden)
+        Ua = self.Ua(encoder_outputs)
+        # expected shape (B x L x encoder_hidden)
 
-        concat = torch.cat((encoder_outputs, decoder_outputs), dim=-1)
+        scores = self.Va(self.activation(Wa + Ua))
+        scores = scores.permute(0, 2, 1)
+        # expected shape (B x 1 x seq_len)
 
-        out = self.fc_1(concat)
-        out = self.activation(out)
+        weights = F.softmax(scores, dim=-1)
+        # expected shape (B x 1 x seq_len)
 
-        scores = self.v_a(out)
-
-        weights = F.softmax(scores, dim=1)
-        context = torch.sum(weights * encoder_outputs, dim=1)
+        context = torch.bmm(weights, encoder_outputs)  # (B x 1 x encoder_hidden)
 
         return context
-
-
-
