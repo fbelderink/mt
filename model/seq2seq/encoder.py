65ebf54fc35ad47c1bdba6523bb8f9a625b064b3
fbelderink <fynnmaarten.business@gmail.com>
2024-06-23 19:03:01 +0200

    introduce gru option and refactored decoder forward

--- model/seq2seq/encoder.py
+++ model/seq2seq/encoder.py
@@ -1,22 +1,44 @@
 import torch.nn as nn
+import torch.nn.functional as F
+from preprocessing.dictionary import PADDING
+from utils.types import RNNType
 
 
 class Encoder(nn.Module):
-    def __init__(self, source_dict_size, embed_dim, lstm_hidden, lstm_layers, lstm_dropout, lstm_bidirectional):
+    def __init__(self,
+                 rnn_type: RNNType,
+                 source_dict_size,
+                 embed_dim,
+                 hidden,
+                 layers,
+                 dropout,
+                 bidirectional):
         super(Encoder, self).__init__()
 
-        self.embedding = nn.Embedding(source_dict_size, embed_dim)
+        self.embedding = nn.Embedding(source_dict_size,
+                                      embed_dim,
+                                      padding_idx=PADDING)
 
-        self.lstm = nn.LSTM(embed_dim,
-                            lstm_hidden,
-                            lstm_layers,
-                            dropout=lstm_dropout,
-                            bidirectional=lstm_bidirectional,
-                            batch_first=True)
+        if rnn_type == RNNType.GRU:
+            self.rnn = nn.GRU(embed_dim,
+                              hidden,
+                              layers,
+                              dropout=dropout,
+                              bidirectional=bidirectional,
+                              batch_first=True)
+        elif rnn_type == RNNType.LSTM:
+            self.rnn = nn.LSTM(embed_dim,
+                               hidden,
+                               layers,
+                               dropout=dropout,
+                               bidirectional=bidirectional,
+                               batch_first=True)
 
     def forward(self, source_sentence):
         embedded = self.embedding(source_sentence)
 
-        outputs, (hidden, cell) = self.lstm(embedded)
+        embedded = F.relu(embedded)
 
-        return outputs, (hidden, cell)
+        outputs, encoder_state = self.rnn(embedded)
+
+        return outputs, encoder_state
