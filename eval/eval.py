4d21d5a22fe128f349bf187a29489ff9320dfee2
fbelderink <fynnmaarten.business@gmail.com>
2024-07-04 19:59:07 +0200

    relocate (irrelevant) eval

--- eval/eval.py
+++ eval/eval.py
@@ -1,64 +1,26 @@
-import random
-
-from preprocessing.BPE import generate_bpe
-from model.seq2seq.recurrent_net import RecurrentNet
-from postprocessing.postprocessing import undo_prepocessing
-from utils.model_hyperparameters import FFModelHyperparameters, RNNModelHyperparameters
-from utils.ConfigLoader import ConfigLoader
-from training.train import train
+import argparse
 import glob
 from utils.file_manipulation import *
 from metrics.calculate_bleu_of_model import get_bleu_of_model
 from search import beam_search
 from scoring import score
 from preprocessing.dictionary import Dictionary
-from preprocessing.dataset.dataset import TranslationDataset
-from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-
-
-def getseeds():
-    seeds = []
-    for i in range(3):
-        seeds.append(random.randint(-(1e10), 1e10))
-    if seeds[0] == seeds[1] or seeds[0] == seeds[2] or seeds[1] == seeds[2]:
-        return getseeds()
-    else:
-        return seeds
-
-
-def execute_runs():
-    for name, seed in zip(["A", "B", "C"], getseeds()):
-        train("data/train7k_w3.pt",
-              "data/val7k_w3.pt",
-              FFModelHyperparameters(ConfigLoader("configs/best_config.yaml").get_config()),
-              max_epochs=5,
-              shuffle=True,
-              num_workers=4,
-              val_rate=0,
-              train_eval_rate=10,
-              random_seed=seed,
-              model_name=name)
 
 
 def get_bleu_of_checkpoints(checkpoint_path: str,
-                            source_data_path: str,
-                            reference_data_path: str,
+                            source_data: List[List[str]],
+                            reference_data: List[List[str]],
                             source_dict: Dictionary,
                             reference_dict: Dictionary,
                             beam_size: int,
-                            window_size: int,
-                            do_beam: bool,
-                            save_path: str):
-    # load data
-    source_data = load_data(source_data_path)
-    reference_data = load_data(reference_data_path)
+                            save_path: str = None):
     # get all model file paths in checkpoint path
     model_file_paths = glob.glob(f"{checkpoint_path}/*.pth")
     # sort for chronological order of checkpoints, if not already
     model_file_paths.sort()
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    data_points = []
+    bleu_scores = []
     for model_path in model_file_paths:
         model = torch.load(model_path, map_location=device)
         # get bleu of model
@@ -68,145 +30,49 @@ def get_bleu_of_checkpoints(checkpoint_path: str,
                                        source_dict,
                                        reference_dict,
                                        beam_size,
-                                       window_size,
-                                       do_beam)
+                                       do_beam_search=True,
+                                       use_torch_bleu=True)
 
-        data_points.append(bleu_score)
-    print(data_points)
-    file = open(save_path, "w")
-    for entry in data_points:
-        file.write(f" {entry} \n")
-    file.close()
-    return data_points
+        bleu_scores.append(bleu_score)
+        print(model_path, bleu_score)
 
+    print(bleu_scores)
 
-def get_ppl_on_model(model: nn.Module,
-                     eval_data_set_path):
+    if save_path is not None:
+        file = open(save_path, "w")
+        for bleu_score in bleu_scores:
+            file.write(f" {bleu_score} \n")
+        file.close()
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    eval_set: TranslationDataset = TranslationDataset.load(eval_data_set_path)
-    eval_dataloader = DataLoader(eval_set, batch_size=200, shuffle=False, num_workers=4)
-    step_counter = 0
-    ppl_list = []
-
-    for S, T, L in eval_dataloader:
-        S = S.to(device)
-        T = T.to(device)
-        L = L.long().to(device)
-
-        pred = model(S, T)
-
-        loss = model.compute_loss(pred, L)
-
-        loss.backward()
-
-        # keep track of metrics
-        step_counter += 1
-
-        # print batch metrics
-        batch_perplexity = float(torch.exp(loss))
-        ppl_list.append(batch_perplexity)
-        print(step_counter / len(eval_dataloader))
-    print(sum(ppl_list) / len(ppl_list))
-    # return averaeg ppl over one epoch
-    return sum(ppl_list) / len(ppl_list)
+    return bleu_scores
 
 
-def get_ppl_on_checkpoint(checkpoint_path: str,
-                          eval_data_set_path,
-                          save_path: str):
+def _parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
 
-    model_file_paths = glob.glob(f"{checkpoint_path}/*.pth")
-    model_file_paths.sort()
-    ppl_list = []
-    for i, model_path in enumerate(model_file_paths):
-        print(i)
-        model = torch.load(model_path)
-        ppl_list.append(get_ppl_on_model(model, eval_data_set_path))
-    print(ppl_list)
+    parser.add_argument('-cp', '--checkpoint_path', type=str)
+    parser.add_argument('-sdp', '--source_data_path', type=str)
+    parser.add_argument('-rdp', '--reference_data_path', type=str)
+    parser.add_argument('-sd', '--source_dict_path', type=str)
+    parser.add_argument('-rd', '--reference_dict_path', type=str)
+    parser.add_argument('-bs', '--beam_size', type=int)
+    parser.add_argument('-sp', '--save_path', type=str)
 
-    file = open(save_path, "w")
-    for ppl in ppl_list:
-        file.write(f"{ppl} \n")
-    file.close()
-
-    return ppl_list
-
-
-def plot_data(data_files_list: List[str],
-              data_description: str,
-              save_path: str):
-
-    data_lists = []
-
-    for i, file_path in enumerate(data_files_list):
-        data = load_data(file_path)
-        data = [float(x[0]) for x in data]
-        data_lists.append(data)
-
-    x_points = [x for x in range(1, len(data_lists[0]) + 1)]
-    print(data_lists[0])
-    plt.figure(figsize=(10, 6))
-    colors = ["b", "r", "g"]
-
-    for i, ydata in enumerate(data_lists):
-        plt.plot(x_points, ydata, marker='o', linestyle='-', color=colors[i])
-
-    plt.xlabel('Checkpoint')
-    plt.ylabel(data_description)
-    plt.grid(True)
-    plt.savefig(save_path)
-
-
-def eval_scores(model: nn.Module,
-                source_data: List[List[str]],
-                target_data: List[List[str]],
-                source_dict: Dictionary,
-                target_dict: Dictionary,
-                beam_size=3,
-                translations=None):
-
-    if translations is None:
-        translations = beam_search.translate(model,
-                                             source_data,
-                                             source_dict,
-                                             target_dict,
-                                             beam_size)
-        translations = undo_prepocessing(translations)  # get_scores expects no bpe applied data
-
-    reference_score = score.get_scores(model,
-                                       source_data,
-                                       target_data,
-                                       source_dict,
-                                       target_dict,
-                                       model.window_size)
-
-    our_score = score.get_scores(model,
-                                 source_data,
-                                 translations,
-                                 source_dict,
-                                 target_dict,
-                                 model.window_size)
-
-    return sum(our_score) / len(our_score), sum(reference_score) / len(reference_score)
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    source_data = load_data('./data/raw/multi30k.dev.de')
-    target_data = load_data('./data/raw/multi30k.dev.en')
+    args = _parse_arguments()
 
-    ops_source = generate_bpe(source_data, 7000)
+    source_data = load_data(args.source_data_path)
+    reference_data = load_data(args.reference_data_path)
+    source_dict = Dictionary.load(args.source_dict_path)
+    reference_dict = Dictionary.load(args.reference_dict_path)
 
-    ops_target = generate_bpe(target_data, 7000)
-
-    source_dict = Dictionary(source_data, ops_source)
-    target_dict = Dictionary(target_data, ops_target)
-
-    config = ConfigLoader("./configs/rnn/config.yaml").get_config()
-    hyperparameters = RNNModelHyperparameters(config)
-    model = RecurrentNet(len(source_dict),
-                         len(target_dict),
-                         hyperparameters)
-
-    a, b = eval_scores(model, source_data, target_data, source_dict, target_dict)
+    get_bleu_of_checkpoints(args.checkpoint_path,
+                            source_data,
+                            reference_data,
+                            source_dict,
+                            reference_dict,
+                            args.beam_size,
+                            args.save_path)
